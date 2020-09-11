69b8ba0ded5abb938f021b84050c1cd7db43bf1e
Vikas Reddy P <vikaspachika@gmail.com>
2020-09-11 06:23:58 +0100

    v4l2test: Create functions for Video device init and close

--- v4l2test.c
+++ v4l2test.c
@@ -21,6 +21,13 @@
  * with this program; if not, write to the Free Software Foundation, Inc.,
  */
 
+/*
+ * v4l2test.c
+ *
+ * 1) Video device init/close functions
+ *
+ *
+ */
 
 #define __STDC_FORMAT_MACROS
 
@@ -48,16 +55,82 @@
 #include <linux/videodev2.h>
 
 
+struct buffer
+{
+	unsigned int idx;
+	unsigned int padding[VIDEO_MAX_PLANES];
+	unsigned int size[VIDEO_MAX_PLANES];
+	void *mem[VIDEO_MAX_PLANES];
+};
+
+struct device
+{
+	int fd;
+	int opened;
+
+	enum v4l2_buf_type type;
+	enum v4l2_memory memtype;
+	unsigned int nbufs;
+	struct buffer *buffers;
+
+	unsigned int width;
+	unsigned int height;
+	uint32_t buffer_output_flags;
+	uint32_t timestamp_type;
+
+	unsigned char num_planes;
+	struct v4l2_plane_pix_format plane_fmt[VIDEO_MAX_PLANES];
+
+	void *pattern[VIDEO_MAX_PLANES];
+	unsigned int patternsize[VIDEO_MAX_PLANES];
+
+	bool write_data_prefix;
+};
+
+
+static void video_init(struct device *dev);
+static void video_close(struct device *dev);
+
 
 int main(int argc, char *argv[])
 {
 
+	struct device dev = { 0 };
+//	int ret;
+
 	printf("Vikas: main() +\n");
 
+	video_init(&dev);
 
 
-	printf("Vikas: main() +\n");
 
+
+	video_close(&dev);
+
+	printf("Vikas: main() -\n");
 	return 0;
 }
+
+
+static void video_init(struct device *dev)
+{
+	dev->fd = -1;
+	dev->memtype = V4L2_MEMORY_MMAP;
+	dev->buffers = NULL;
+	dev->type = (enum v4l2_buf_type)-1;
+}
+
+static void video_close(struct device *dev)
+{
+	unsigned int i;
+
+	for (i = 0; i < dev->num_planes; i++)
+		free(dev->pattern[i]);
+
+	free(dev->buffers);
+	if (dev->opened)
+		close(dev->fd);
+}
+
+
 
